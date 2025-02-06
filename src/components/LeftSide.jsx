fcb631a3fa06b3c18d17949e4c76ac0ca27de091
Saru <saritasharmaprajuli@gmail.com>
2025-02-06 12:38:42 +0545

    instagram

---
+++ src/components/LeftSide.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { AtSign, Bell, Compass, Film, House, Menu, MessageCircleCode, Search, SquarePlus, User } from 'lucide-react';
+export default function LeftSide() {
+    return (
+        <div className='bg-gray-50 px-4'>
+            <p>Instagram</p>
+
+            <div className='flex flex-col justify-between h-[96vh]'>
+                <div>
+                    {
+                        linksTop.map((eachItem, index) => (
+
+
+                            <div key={index} className='flex items-center gap-2 font-semibold py-2 hover:bg-gray-200 cursor-pointer px-2 rounded-md opacity-80'>
+                                {eachItem.icon}
+                                <p>{eachItem.title}</p>
+                            </div>
+                        ))
+                    }
+
+
+                </div>
+                <div>
+                {
+                        linksButton.map((eachItem, index) => (
+
+
+                            <div key={index} className='flex items-center gap-2 font-semibold py-2 hover:bg-gray-200 cursor-pointer px-2 rounded-md opacity-80'>
+                                {eachItem.icon}
+                                <p>{eachItem.title}</p>
+                            </div>
+
+
+
+                        ))
+
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+const linksTop = [
+
+    {
+        icon: <House />,
+        title: "Home",
+    },
+
+    {
+        icon: <Search />,
+        title: "Search",
+    },
+
+    {
+        icon: <Compass />,
+        title: "Explore",
+    },
+
+    {
+        icon: <Film />,
+        title: "Reels",
+    },
+
+    {
+        icon: <MessageCircleCode />,
+        title: "Message",
+    },
+
+    {
+        icon: <Bell />,
+        title: "Notification",
+    },
+
+    {
+        icon: <SquarePlus />,
+        title: "Create",
+    },
+
+
+    {
+        icon: <User />,
+        title: "Profile",
+    },
+
+]
+
+
+
+const linksButton = [
+
+
+    {
+        icon: <AtSign />,
+        title: "Threads",
+    },
+
+    {
+        icon: <Menu />,
+        title: "More",
+    },
+
+
+]
---
+++ src/components/RightSide.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+export default function RightSide() {
+    return (
+        <div className='bg-gray-50 px-4'>
+            <div className='flex items-center justify-between font-semibold opacity-80'>
+                <p>Suggested for you</p>
+                <p className='text-blue-500 cursor-pointer'>See All</p>
+            </div>
+
+            <div className='space-y-4'>
+                {
+                    people.map((eachItem, index) => (
+
+                        <div key={index} className='flex items-center justify-between'>
+                            <div className='flex gap-2'>
+                                <img height={50} width={50} className="rounded-full object-cover" src='https://images.unsplash.com/photo-1511367461989-f85a21fda167?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D' alt='' />
+                                <div>
+                                    <p className='text-lg font-semibold'>{eachItem.username}</p>
+                                    <p className='opacity-60'>Followed by{eachItem.followedBy}</p>
+
+                                </div>
+                            </div>
+                            <p className="text-blue-500 font-semibold">Follow</p>
+                        </div>
+
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+
+const people = [
+
+    {
+        profilepicture: "https://images.unsplash.com/photo-1511367461989-f85a21fda167?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+        username: "Sarita Sharma",
+        followedBy: " Kripa Khatiwada",
+    },
+    {
+        profilepicture: "https://images.unsplash.com/photo-1511367461989-f85a21fda167?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+        username: "Sarita Sharma",
+        followedBy: "Kripa Khatiwada",
+    },
+
+    {
+        profilepicture: "https://images.unsplash.com/photo-1511367461989-f85a21fda167?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+        username: "absbh",
+        followedBy: ",sjdkwi",
+    },
+
+    {
+        profilepicture: "https://images.unsplash.com/photo-1511367461989-f85a21fda167?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+        username: "jsdkwe",
+        followedBy: "majnkdj",
+    },
+
+    {
+        profilepicture: "https://images.unsplash.com/photo-1511367461989-f85a21fda167?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+        username: "jahkjd",
+        followedBy: "nmABSQUWH",
+    },
+
+
+]
