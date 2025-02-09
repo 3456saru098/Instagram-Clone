6698fe2c94705ae3635c1ef6de4a9d0e7b98dbee
Saru <saritasharmaprajuli@gmail.com>
2025-02-09 11:41:11 +0545

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import LeftSide from './components/LeftSide'
-import MiddleArea from './components/MiddleArea'
-import RightSide from './components/RightSide'
+import React from "react";
+import LeftSide from "./components/LeftSide";
+import MiddleArea from "./components/MiddleArea";
+import RightSide from "./components/RightSide";
 
 export default function App() {
   return (
-    <div className='grid grid-cols-3'>
-      <LeftSide/>
-      <MiddleArea/>
-      <RightSide/>
+    <div className="flex  relative h-screen">
+      <div className="sticky top-0 h-screen w-2/12">
+        <LeftSide />
+      </div>
+
+      <div className="overflow-y-auto h-screen w-6/12">
+        <MiddleArea />
+      </div>
       
+        <div className="sticky top-0 h-screen bg-gray-50 w-4/12">
+          <RightSide />
+        </div>
+   
     </div>
-  )
+  );
 }
